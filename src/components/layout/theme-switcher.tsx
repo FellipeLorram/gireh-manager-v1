112e6c0f2e40f52e6fe970701e8928c9a750b163
Fellipe Lorram <fellipelorram@gmail.com>
2023-11-08 20:53:20 -0300

    Refactor theme switcher component to use dynamic loading for Lucide icons.

--- src/components/layout/theme-switcher.tsx
+++ src/components/layout/theme-switcher.tsx
@@ -1,19 +1,20 @@
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import dynamic from 'next/dynamic';
+import { useTheme } from 'next-themes';
+
+// Wrap Lucide components with dynamic to load them only on the client side
+const DynamicSun = dynamic(() => import('lucide-react').then((module) => module.Sun), { ssr: false });
+const DynamicMoon = dynamic(() => import('lucide-react').then((module) => module.Moon), { ssr: false });
 
 export function ThemeSwitcher() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <button
+    <div
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       className="rounded-full w-10 h-10 flex items-center justify-center border cursor-pointer duration-200 hover:bg-muted-foreground/10 mr-2"
     >
-      {theme === "dark" ? (
-        <Sun className="w-5 h-5 stroke-foreground" />
-      ) : (
-        <Moon className="w-5 h-5 stroke-foreground" />
-      )}
-    </button>
-  )
+      {theme === 'dark' && <DynamicSun className="w-5 h-5 stroke-foreground" />}
+      {theme === 'light' && <DynamicMoon className="w-5 h-5 stroke-foreground" />}
+    </div>
+  );
 }
