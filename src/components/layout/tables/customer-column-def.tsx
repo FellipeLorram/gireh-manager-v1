1d52f1da8d75ae8219665fe73137a5345bee567d
Fellipe Lorram <fellipelorram@gmail.com>
2023-11-09 14:45:53 -0300

    Add customer table column definition and raw table component

---
+++ src/components/layout/tables/customer-column-def.tsx
@@ -0,0 +1,79 @@
+import { type CellContext, type ColumnDef } from "@tanstack/react-table"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../../ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { useToast } from "../../ui/use-toast";
+import { api } from "@/utils/api";
+import Link from "next/link";
+import { type Customer } from "@prisma/client";
+
+
+function DropdownCell({ row }: CellContext<Customer, unknown>) {
+	const { id, inLine } = row.original
+	const { toast } = useToast();
+	const { mutate } = api.customer.update.useMutation({
+		onSuccess: () => {
+			toast({
+				title: 'Cliente adicionado a fila de exame',
+			})
+		}
+	});
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger
+				className='group'
+			>
+				<MoreHorizontal
+					className='stroke-muted-foreground group-hover:stroke-foreground duration-200'
+					size={20}
+				/>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem
+					asChild
+					className='text-sm p-2 px-3 text-white cursor-pointer'
+				>
+					<Link href={`/customers/${id}/new-order`}>
+						Seguir para Venda
+					</Link>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					onClick={() => {
+						mutate({ id, inLine: !inLine });
+					}}
+					className='text-sm p-2 px-3 text-white cursor-pointer'
+				>
+					{inLine ? 'Remover da fila' : 'Adicionar a fila'}
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+
+	)
+}
+
+export const customerColumnDef: ColumnDef<Customer>[] = [
+	{
+		accessorKey: "createdAt",
+		header: "Data",
+		cell: ({ row }) => {
+			const date = new Date(row.original.createdAt);
+			return date.toLocaleDateString();
+		}
+	},
+	{
+		id: "name",
+		accessorKey: "name",
+		header: "Nome",
+		cell: ({ row }) => <Link href={`/customers/${row.original.id}`}>{row.original.name}</Link>,
+		filterFn: (row, _, filterValue) => {
+			const name = row.original.name.toLowerCase();
+			const filter = filterValue as string;
+			return name.startsWith(filter.toLowerCase());
+		}
+	},
+	{
+		accessorKey: "actions",
+		header: '',
+		cell: DropdownCell,
+	},
+]
---
+++ src/components/layout/tables/table-raw.tsx
@@ -0,0 +1,133 @@
+import {
+	type ColumnDef,
+	flexRender,
+	getCoreRowModel,
+	getPaginationRowModel,
+	useReactTable,
+	type ColumnFiltersState,
+	getFilteredRowModel,
+} from "@tanstack/react-table"
+
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table"
+
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { useState } from "react"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[];
+	data: TData[];
+	filterField?: string;
+}
+
+export function DataTable<TData, TValue>({
+	columns,
+	data,
+	filterField,
+}: DataTableProps<TData, TValue>) {
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+		[]
+	);
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		onColumnFiltersChange: setColumnFilters,
+		getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			columnFilters,
+		},
+	})
+
+	return (
+		<div className="w-full flex flex-col items-start justify-center gap-2">
+			{filterField && (
+				<div className="flex flex-row items-center justify-between w-full mb-2">
+					<Input
+						className="w-full max-w-full"
+						placeholder="Buscar..."
+						value={(table.getColumn(filterField)?.getFilterValue() as string) ?? ""}
+						onChange={(event) => {
+							table.getColumn(filterField)?.setFilterValue(event.target.value)
+						}}
+					/>
+				</div>
+			)}
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder
+											? null
+											: flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+											)}
+									</TableHead>
+								)
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow
+
+								key={row.id}
+								data-state={row.getIsSelected() && "selected"}
+							>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24 text-center">
+								Não Encontrado.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+			{data.length > 10 && (
+				<div className="flex flex-row items-center justify-end w-full gap-2">
+					<Button
+						onClick={() => table.previousPage()}
+						disabled={!table.getCanPreviousPage()}
+						variant="outline"
+						className="flex items-center justify-center"
+					>
+						<ChevronLeft size={16} />
+						<span className="text-sm">Anterior</span>
+					</Button>
+					<Button
+						variant="outline"
+						className="flex items-center justify-center"
+						onClick={() => table.nextPage()}
+						disabled={!table.getCanNextPage()}
+					>
+						<span className="text-sm">Próximo</span>
+						<ChevronRight size={16} />
+					</Button>
+				</div>
+			)}
+		</div>
+	)
+}
