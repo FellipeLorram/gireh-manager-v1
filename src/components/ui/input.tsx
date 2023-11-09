37299d5c61b002c89ac17e33be1ef2400afd6116
Fellipe Lorram <fellipelorram@gmail.com>
2023-11-09 13:17:22 -0300

    Add UI components for toast, checkbox, input, and table

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,22 @@
-import * as React from "react"
-
 import { cn } from "@/lib/utils"
+import { forwardRef } from "react";
+import InputMask, { type Props as InputMaskProps } from 'react-input-mask';
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends Omit<InputMaskProps, 'mask'> {
+  mask?: string;
+}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, mask, type, ...props }, ref) => {
     return (
-      <input
+      <InputMask
+        mask={mask ?? ""}
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        ref={ref}
+        inputRef={ref}
         {...props}
       />
     )
