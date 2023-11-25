826388621c503f87a2baf9902f4adba2f73f5396
Fellipe Lorram <fellipelorram@gmail.com>
2023-11-25 11:33:43 -0300

    Add PWA support

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 await import("./src/env.mjs");
+import pwa from 'next-pwa';
+
+const withPWA = pwa({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config = withPWA({
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["pt-BR"],
+    defaultLocale: "pt-BR",
   },
-};
+});
 
 export default config;
