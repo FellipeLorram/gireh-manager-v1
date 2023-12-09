abc060305251c014e31415ad8259f61b75be144d
Fellipe Lorram <fellipelorram@gmail.com>
2023-12-09 16:51:08 -0300

    Refactor topbar component and add new pages for orgs, profile, and team

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -1,9 +1,20 @@
-import { Bell, UserCircle } from "lucide-react";
+import { UserCircle } from "lucide-react";
 import Link from "next/link";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 import { buttonVariants } from "../ui/button";
 import { ThemeSwitcher } from "./theme-switcher";
+import { signOut } from "next-auth/react";
 
 export function Topbar() {
+	
 	return (
 		<div className="w-full flex items-center justify-between">
 			<div className="flex-1">
@@ -14,15 +25,27 @@ export function Topbar() {
 				</Link>
 			</div>
 
-			<div className="hidden md:block">
-				<ThemeSwitcher />
-			</div>
-			<div className="rounded-full w-10 h-10 flex items-center justify-center border cursor-pointer duration-200 hover:bg-muted-foreground/10 mx-2">
-				<Bell className="w-5 h-5 stroke-foreground" />
-			</div>
-			<div className="rounded-full w-10 h-10 flex items-center justify-center border cursor-pointer duration-200 hover:bg-muted-foreground/10">
-				<UserCircle className="w-5 h-5 stroke-foreground" />
-			</div>
+			<ThemeSwitcher />
+			<DropdownMenu>
+				<DropdownMenuTrigger>
+					<div className="rounded-full w-10 h-10 flex items-center justify-center border cursor-pointer duration-200 hover:bg-muted-foreground/10">
+						<UserCircle className="w-5 h-5 stroke-foreground" />
+					</div>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>Perfil</DropdownMenuItem>
+					<DropdownMenuItem>
+						<Link href="/settings/orgs">
+							Óticas
+						</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem>Time</DropdownMenuItem>
+					<DropdownMenuItem onClick={() => signOut()}>Sair</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+
 		</div>
 	)
 }
---
+++ src/pages/settings/orgs/[orgid].tsx
@@ -0,0 +1,26 @@
+import { api } from "@/utils/api"
+import { ArrowLeftCircle } from "lucide-react";
+import Link from "next/link";
+
+export default function Page() {
+	const { data } = api.org.listUserOrgs.useQuery();
+
+	return (
+		<div className="mx-auto w-11/12 max-w-3xl min-h-screen flex flex-col items-center justify-start py-4 gap-4">
+			<div className="w-full flex flex-row">
+				<Link href="/">
+					<ArrowLeftCircle className="w-8 h-8 stroke-muted-foreground hover:stroke-foreground duration-200" />
+				</Link>
+
+				<h1 className="text-xl text-center mx-auto">Óticas</h1>
+			</div>
+
+			<div className="w-full flex flex-wrap">
+				{data?.map((org) => (
+					<></>
+				))}
+
+			</div>
+		</div>
+	)
+}
