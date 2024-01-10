1ef717cb0c14d0160b35640036c8cfef859fca75
Fellipe Lorram <fellipelorram@gmail.com>
2024-01-10 10:26:53 -0300

    Add create-customer use case and test

---
+++ src/server/use-cases/customer/create-customer.spec.ts
@@ -0,0 +1,35 @@
+import { createCustomerInput, createCustomerUseCase } from './create-customer';
+import { prismaMock } from '../../test/assets/singleton';
+
+describe('Create Customer Use Case', () => {
+	it('should create a customer with phone', async () => {
+		const input = createCustomerInput.parse({
+			name: 'John Doe',
+			phone: [{ number: '1234567890' }],
+			address: '123 Main St',
+			inLine: true,
+			birthDate: '2000-01-01',
+			orgId: 'org1',
+		});
+
+		await createCustomerUseCase({ prisma: prismaMock, input });
+
+		expect(prismaMock.customer.create).toHaveBeenCalledWith({
+			data: {
+				birthDate: new Date('2000-01-01'),
+				name: input.name,
+				address: input.address,
+				age: new Date().getFullYear() - new Date('2000-01-01').getFullYear(),
+				inLine: input.inLine,
+				orgId: input.orgId,
+				Phone: {
+					create: input.phone?.map((phone) => ({
+						number: phone.number,
+					})),
+				},
+			},
+		});
+	});
+});
+
+
---
+++ src/server/use-cases/customer/create-customer.ts
@@ -0,0 +1,42 @@
+import { type PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+
+export const createCustomerInput = z.object({
+	name: z.string().min(3, 'Nome inválido'),
+	phone: z.array(z.object({
+		number: z.string(),
+	})).optional(),
+	address: z.string().optional(),
+	inLine: z.boolean().optional(),
+	birthDate: z.string().optional(),
+	orgId: z.string(),
+});
+
+export async function createCustomerUseCase({
+	prisma,
+	input,
+}: {
+	prisma: PrismaClient;
+	input: z.infer<typeof createCustomerInput>;
+}) {
+	const birthDate = input.birthDate ? new Date(input.birthDate) : null;
+	const age = input.birthDate ? new Date().getFullYear() - new Date(input.birthDate).getFullYear() : null;
+
+	const customer = await prisma.customer.create({
+		data: {
+			birthDate: birthDate,
+			name: input.name,
+			address: input.address,
+			age: age,
+			inLine: input.inLine,
+			orgId: input.orgId,
+			Phone: {
+				create: input.phone?.map((phone) => ({
+					number: phone.number,
+				})),
+			},
+		},
+	});
+
+	return customer;
+}
