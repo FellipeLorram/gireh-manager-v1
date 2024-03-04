ac1215df286b178165b8b68abe69196d8fdf80c9
Fellipe Lorram <fellipelorram@gmail.com>
2024-03-04 19:45:36 -0300

    Refactor DashboardLayout component

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -6,16 +6,12 @@ export function DashboardLayout(props: {
 	children: React.ReactNode
 }) {
 	return (
-		// <main className='w-full h-screen overflow-hidden flex flex-row items-start justify-start'>
-		// 	<Navbar />
-		// 	<NavbarMobile />
 		<main className="w-full min-h-screen">
 			<Topbar />
-			<div className={cn("mx-auto w-11/12 max-w-7xl pt-4 md:pt-8", props.className)}>
+			<div className={cn("mx-auto w-11/12 max-w-7xl pt-4", props.className)}>
 				{props.children}
 			</div>
 			<div className="h-20"></div>
 		</main>
-		// </main>
 	)
 }
