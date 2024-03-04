c4e6413c93c8ed5527f952ccae246eb7407b739b
Fellipe Lorram <fellipelorram@gmail.com>
2024-03-04 19:44:31 -0300

    Update font to Inter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Montserrat } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
 import { api } from "@/utils/api";
 
@@ -10,9 +10,9 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { AuthProvider } from "@/components/providers/auth-provider";
 import { Toaster } from "@/components/ui/toaster";
 
-const montserrat = Montserrat({
+const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-montserrat',
+  variable: '--font-inter',
 })
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -24,18 +24,22 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <style jsx global>
         {`
             :root {
-              --font-montserrat: ${montserrat.variable}
+              --font-inter: ${inter.variable}
             }
           
           `}
       </style>
       <SessionProvider session={session}>
-        <main className={`${montserrat.variable} font-sans`} >
-          <AuthProvider>
-            <Component {...pageProps} />
-            <Toaster />
-          </AuthProvider>
-        </main>
+        <ThemeProvider
+          attribute="class" enableSystem defaultTheme="system"
+        >
+          <main className={`${inter.variable} font-sans`} >
+            <AuthProvider>
+              <Component {...pageProps} />
+              <Toaster />
+            </AuthProvider>
+          </main>
+        </ThemeProvider>
       </SessionProvider>
     </>
   );
